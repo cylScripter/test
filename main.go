5a8f3ac2151298c979aacd62d521675b6c0e8259
cyl <2842871262@qq.com>
2024-12-16 21:34:48 +0800

    fix

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+
+	fmt.Println()	
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	
+
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+
+
 }
